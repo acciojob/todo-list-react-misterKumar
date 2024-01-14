0ff2033b0816e7479a800efd1cca22ed77e79f3b
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-14 08:27:51 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,86 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-import TodoApp from "./TodoApp";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleAdd = () => {
+    if (taskInput.trim() !== "") {
+      const updatedTasks = [...tasks];
+      if (editIndex !== -1) {
+        updatedTasks[editIndex] = taskInput;
+      } else {
+        updatedTasks.push(taskInput);
+      }
+
+      setTasks(updatedTasks);
+      setTaskInput("");
+      setEditIndex(-1);
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+    setEditIndex(-1);
+  };
+
+  const handleEdit = (index) => {
+    setTaskInput(tasks[index]);
+    setEditIndex(index);
+  };
+
+  const handleUpdate = () => {
+    handleAdd();
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <TodoApp />
+    <div className="add_tasks_section">
+      <h1>To Do App</h1>
+      <input
+        type="text"
+        name="task"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+      />
+      <button onClick={handleAdd}>
+        {editIndex !== -1 ? "Save" : "Add Task"}
+      </button>
+
+      <ul className="tasks_section">
+        {tasks.map((task, index) => (
+          <li className="task" key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={taskInput}
+                  onChange={(e) => setTaskInput(e.target.value)}
+                />
+                <button className="save" onClick={handleUpdate}>
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                {task}
+                <button className=".edit" onClick={() => handleEdit(index)}>
+                  Edit
+                </button>
+                <button className=".delete" onClick={() => handleDelete(index)}>
+                  Delete
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
